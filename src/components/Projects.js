4849ef5e9c5eb59f0d52b816f27aeed4cc96f09d
heremyas <jhem2911@gmail.com>
2021-04-30 17:13:32 +0800

    added jemtech to projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -41,7 +41,36 @@ function Projects() {
             React skill. I use the trefle plant API to gather data and React JS
             for the main U/I. Click the link to try it.{" "}
             <span>
-              <a href="https://botanim.netlify.app/">
+              <a
+                href="https://botanim.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <strong>Preview here</strong>
+              </a>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="projects_container projects_container_odd">
+        <div className="img_container">
+          <img alt="Project Thumbnail" src="https://i.imgur.com/qSfYQwD.png" />
+        </div>
+        <div className="project_details_container">
+          <p className="project_title">JEM TECH</p>
+          <p>
+            <span>
+              <strong>Description: </strong>
+            </span>{" "}
+            Jem Tech is a blog site of mine. I use NEXTJS for the Frontend and
+            DJANGO for the Backend. In this project, I learned how to create a
+            Server and connect it in a Client.{" "}
+            <span>
+              <a
+                href="https://jemtech.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <strong>Preview here</strong>
               </a>
             </span>
