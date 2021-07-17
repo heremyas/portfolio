5cc6866977febc8759ad7f9a217ca35bdbbdbcba
heremyas <jhem2911@gmail.com>
2021-07-17 21:05:29 +0800

    changed profile line

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -22,7 +22,7 @@ function Intro() {
             <img
               alt="myimage"
               className="myImage"
-              src="https://scontent.fcrk1-1.fna.fbcdn.net/v/t1.6435-9/147454798_5042041179203023_1862199331036992512_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHFJUko907LPxerP_uVzzvhBNjeZwestDUE2N5nB6y0Nbwfq61pHIiwwjXxUM9nSMDNEBISA3MFXpBgCyputGIu&_nc_ohc=kOmn_vRo0c0AX_TVyqP&_nc_ht=scontent.fcrk1-1.fna&oh=d0e0382bdc66d0b18f60d0a8dbc8f0d5&oe=60AE2376"
+              src="https://scontent.fcrk1-1.fna.fbcdn.net/v/t1.6435-9/147454798_5042041179203023_1862199331036992512_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHFJUko907LPxerP_uVzzvhBNjeZwestDUE2N5nB6y0Nbwfq61pHIiwwjXxUM9nSMDNEBISA3MFXpBgCyputGIu&_nc_ohc=QIH_Z2wm6sYAX-Q04LY&_nc_ht=scontent.fcrk1-1.fna&oh=336869968b6ea428ed0072c7c38db6b4&oe=60F74EB6"
             ></img>
           </div>
         </div>
